4214d782f12681d34385315ea15e9fe7cb817059
Saverio Porcari <saverio.porcari@softwell.it>
2015-12-02 13:15:41 +0100

    added aliascolumn

--- packages/cnk/model/album.py
+++ packages/cnk/model/album.py
@@ -6,3 +6,4 @@ class Table(object):
         tbl.column('albumid',dtype='I',name_long='albumid',indexed=True,legacy_name='AlbumId',unique=True)
         tbl.column('title',size='0:160',name_long='title',legacy_name='Title')
         tbl.column('artistid',dtype='I',name_long='artistid',indexed=True,legacy_name='ArtistId',unique=False).relation('artist.artistid',onDelete='raise')
+        tbl.aliasColumn('artist_name', relation_path='@artistid.name', name_long='Artist')
